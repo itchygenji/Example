48ed06eabf6a0d425124be6406d821e91a338dd6
Jean Bonefont <jbonefont@Jeans-MacBook-Pro-2.local>
2020-01-17 18:46:56 -0500

    HW

---
+++ My First Project/src/FirstProgram.java
@@ -0,0 +1,41 @@
+
+
+
+import java.util.Scanner;
+
+public class FirstProgram {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.printf("hello world!%n");
+		System.out.print("hello world again everyone!\n");
+		int age = 12; 
+		double weight = 123.345678;
+		System.out.printf("hello everyone!%nI am %d years old%n", age);
+		System.out.printf("I weigh %.2f\n", weight);
+		
+
+		int x = 12;
+		int y = 10;
+		
+		double u = 10.0;
+		System.out.println(x / 0.001);
+		System.out.println(y / u);
+		System.out.println(12.0 % 10.5);*/
+		
+		Scanner input = new Scanner(System.in);
+		
+		int num;
+		
+		num = input.nextLine();
+		
+		System.out.println("input :", num);
+		
+		if((num % 3)== 0) System.out.println();
+		
+		
+
+	}
+
+}
